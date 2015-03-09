237a74ddc12797bcc3d861f6180eee5900c9d3ed
kingjr <jeanremi.kibng+github@gmail.com>
2015-03-09 10:02:41 +0100

    correct libraries 2

--- scripts/run_ica.py
+++ scripts/run_ica.py
@@ -5,9 +5,7 @@ import os.path as op
 
 from mne.io.pick import pick_types as picks_by_type
 from mne.io import Raw
-
-from meeg_preprocessing import compute_icagit fil
-
+from meeg_preprocessing import compute_ica
 from meeg_preprocessing.utils import setup_provenance, set_eog_ecg_channels
 
 from config import (

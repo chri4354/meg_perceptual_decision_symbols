f37b58e15a1bfbc1287ed9ce1485aff4001910ad
J-R King <jeanremi.king+github@gmail.com>
2015-03-28 21:11:56 +0100

    Update events_subject05_run03.py

--- sandbox/events_subject05_run03.py
+++ sandbox/events_subject05_run03.py
@@ -7,5 +7,6 @@ raw = mne.io.Raw(data_path+fname, preload=True)
 S_ch = ['STI001', 'STI002', 'STI003', 'STI004', 'STI005', 'STI006']
 M_ch = ['STI013', 'STI015']
 raw.pick_channels(S_ch + M_ch)
+
 events = mne.find_events(raw, stim_channel='STI101', consecutive='increasing',
                         min_duration=0.000, verbose=True)
